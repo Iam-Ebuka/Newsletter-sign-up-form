aebc3a2309fc3492d2d54217f44684cb62d39cf0
Iam-Ebuka <ebukagrizz@gmail.com>
2024-01-22 18:19:08 +0100

    done the apps responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,36 @@ const App: React.FC = () => {
  
 
   return (
-    <div className="text-[16px] container w-full mx-auto">
-      <div className='flex w-full' >
-        <img src={mobileSvg} alt="mobile image" className=""/>
+    <div className="text-[16px] container w-full mx-auto md:px-5 md:max-w-[1050px]  md:flex md:justify-between md:flex-row-reverse md: md:gap-[30px]">
+      <div className='md:h-screen md:flex md:items-center md:align-middle ' >
+        <img src={mobileSvg} alt="mobile image" className="md:hidden"/>
+        <img src={desktopSvg} alt="desktop image" className="hidden md:block md:h-[90%]" />
       </div>
-      <div className="p-5">
-        <h1 className="text-[30px] font-bold pb-3">Stay updated!</h1>
-        <p className="pb-5">Join 60,000+ product managers receiving monthly updates on:</p>
-        <div className="space-y-2">
-          <div className="flex items-start align-middle gap-3">
-            <img src={list} alt="list" />
-            <p>Product discovery and building what matters</p>
+      <div className="p-5 mt-3 
+      font-[600] md:mt-0 
+      md:flex
+      md:items-center 
+      md:h-screen md:text-[20px] md:max-w-[500px]">
+        <div>
+          <h1 className="text-[38px] font-bold pb-3 md:text-[70px]">Stay updated!</h1>
+          <p className="pb-5">Join 60,000+ product managers receiving monthly updates on:</p>
+          <div className="space-y-2">
+            <div className="flex items-start align-middle gap-3">
+              <img src={list} alt="list" />
+              <p>Product discovery and building what matters</p>
+            </div>
+            <div className="flex items-start align-middle gap-3">
+              <img src={list} alt="list" />
+              <p>Measuring to ensure updates are a success</p>
+            </div>
+            <div className="flex items-start align-middle gap-3">
+              <img src={list} alt="list" />
+              <p>And much more!</p>
+            </div>
           </div>
-          <div className="flex items-start align-middle gap-3">
-            <img src={list} alt="list" />
-            <p>Measuring to ensure updates are a success</p>
-          </div>
-          <div className="flex items-start align-middle gap-3">
-            <img src={list} alt="list" />
-            <p>And much more!</p>
-          </div>
-        </div>
 
-        <Email />
+          <Email />
+        </div>
       </div>
     </div>
   )
--- src/Email.tsx
+++ src/Email.tsx
@@ -1,27 +1,47 @@
 import React, { useState } from 'react'
 
 const Email: React.FC = () => {
-    const [value, setValue] = useState<string>("")
+    const [value, setValue]: [string, React.Dispatch<React.SetStateAction<string>>] = useState<string>("")
+    const [error, setError] = useState<boolean>(false)
 
-    const handleChange = (): void => {
-        console.log("hello")
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        e.preventDefault();
+        setValue(e.target.value);
+        setError(false);
+    }
+    const handleSubmit = () => {
+        if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) === false) {
+            setError(true);
+        } else {
+
+        }
     }
 
   return (
     <div className='mt-8'>
-        <form>
-            <label className='text-[12px] block font-bold mb-2'>Email address</label>
+        <div >
+            <div className='text-[12px] font-bold mb-2 flex justify-between'>
+            <label className=''>Email address</label>
+            {error && <p className='text-red-500'>Valid email required</p>}
+            </div>
+            
             <input 
-            className='border rounded-lg border-[#c3bdbd] w-full h-[60px] text-[16px] pl-6' 
+            className={`border 
+            rounded-lg
+            w-full 
+            h-[60px] 
+            text-[16px] 
+            pl-6
+            ${error ? 'border-red-500 bg-red-200 text-red-500' : 'border-[#c3bdbd]' }`}  
             type='email' 
             placeholder='email@company.com'
             value={value}
             onChange={handleChange}/>
             
-            <button className='bg-[#1a1f3c] rounded-lg py-5 w-full text-white mt-6'>
+            <button onClick={handleSubmit} className='bg-[#1a1f3c] hover:bg-[#040717] rounded-lg px-1 py-5 w-full text-white mt-6'>
                 Subscribe to monthly newsletter
             </button>
-        </form>
+        </div>
 
 
 
